9fe46603a82d78a2eceb7dd295c0df59491c97b8
xfoxolx <107661287+xfoxolx@users.noreply.github.com>
2025-04-11 18:53:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,11 +15,11 @@
 
 # Add a feed source
 # add vpn
-echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> feeds.conf.default
+echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' >> feeds.conf.default
 # add store
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 # add theme
-echo 'src-git alpha_theme: https://github.com/xfoxolx/luci-theme-alpha;master' >> feeds.conf.default
+echo 'src-git alpha_theme https://github.com/xfoxolx/luci-theme-alpha;master' >> feeds.conf.default
 echo "src-git alpha_config https://github.com/derisamedia/luci-app-alpha-config;master" >> feeds.conf.default
 
 
